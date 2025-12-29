b66382a3f0d25af5474be61b0d06efe646df3d6d
TAN <tian_naixiang@sina.com>
2025-12-29 21:45:12 +0800

    -

--- Riz.DotNetCoreDemo.Utils/AppUtils.cs
+++ Riz.DotNetCoreDemo.Utils/AppUtils.cs
@@ -4,6 +4,6 @@ namespace Riz.DotNetCoreDemo.Utils
 {
     public class AppUtils
     {
-        public static string GetEnviromentName() => "DEV";
+        public static string GetEnviromentName() => "DEV22";
     }
 }
